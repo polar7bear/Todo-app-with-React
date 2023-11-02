6869cd99c05cb663eb3c7f68700c581b07544935
solarbear <sons1998@naver.com>
2023-11-02 16:20:31 +0900

    state 이욯하여 변경사항 렌더링하기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 export default class App extends Component {
 
+  
   btnStyle = {
     color: "fff",
     border: "none",
@@ -10,28 +11,40 @@ export default class App extends Component {
     borderRadius: "50%",
     cursor: "pointer",
     float: "right"
-  }
-
+  };
+  
   getStyle = () => {
     return {
       padding: "10px",
       borderBottom: "1px #ccc dotted",
       textDecoration: "none"
     }
+  };
+  
+  
+  state = { //React State
+            //리액트에서는 화면 렌더링이 변경될 수 있게 state를 사용함.
+    todoData: [
+      {
+        id: "1",
+        title: "공부하기",
+        completed: true
+      },
+      {
+        id: "2",
+        title: "청소하기",
+        completed: false
+      }
+    ]
+  }
+  
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter(data => data.id !== id);
+    console.log('newTodoData', newTodoData);
+    this.setState({todoData: newTodoData});
   }
 
-  todoData = [
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true
-    },
-    {
-      id: "2",
-      title: "청소하기",
-      completed: false
-    }
-  ]
+
   render() {  // Class Component에서는 render() 함수 안에서 ui를 작성해야 한다.
     return(
       <div className="container">    {/* 이와 같이 함수 안에 html 문법을 작성 하는것을 jsx라고 한다. */}
@@ -40,13 +53,13 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
 
-          {this.todoData.map((data) => (
+          {this.state.todoData.map((data) => (
             <div style={this.getStyle()} key={data.id}> 
             {/* 리액트에서 요소의 리스트를 나열 할 때는 key를 넣어줘야한다.
               jsx key를 명시함으로써 변경, 추가 및 제거 된 항목을 식별하는데 도움이 됨. */}
               <input type="checkbox" defaultChecked={false} />
               {data.title}
-              <button style={this.btnStyle}>x</button>
+              <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
             </div>
 
           ))}
