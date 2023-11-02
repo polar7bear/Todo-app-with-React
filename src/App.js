0b5c589de5fc8fcb671675963ee1dbef2e45a003
solarbear <sons1998@naver.com>
2023-11-02 14:58:32 +0900

    기본 구성 갖추기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {  // Class Component에서는 render() 함수 안에서 ui를 작성해야 한다.
+    return(
+      <div className="container">    {/* 이와 같이 함수 안에 html 문법을 작성 하는것을 jsx라고 한다. */}
+        <div className="todoBlock">  {/* 리액트의 barbel이 이러한 jsx문법을 createElement 형식으로 변환해줌 */}
+          <div className="title">   {/* jsx에서는 className이라고 명시해야 한다. */}
+            <h1>할 일 목록</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
