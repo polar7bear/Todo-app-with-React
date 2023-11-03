b0ebebf25c88c36fc62ace45e9573e018e0033a8
solarbear <sons1998@naver.com>
2023-11-03 16:50:40 +0900

    tailwind css initialized

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ export default function App() {
           <h1>할 일 목록</h1>
         </div>
 
+        <h1 className="text-3xl font-bold underline">Hello World!</h1>
         <List todoData={todoData} setTodoData={setTodoData} />
 
         <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
